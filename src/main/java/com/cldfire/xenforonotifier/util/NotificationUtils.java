20979244ae4cc944f6fea52c3e333b77bda563a9
Scott <scott-scheiner@hotmail.com>
2016-04-22 21:41:39 +0930

    Rename Spigot to XenForo and fix duplicating the TrayIcon

--- src/main/java/com/cldfire/xenforonotifier/util/NotificationUtils.java
+++ src/main/java/com/cldfire/xenforonotifier/util/NotificationUtils.java
@@ -8,17 +8,28 @@ import java.awt.TrayIcon.MessageType;
 
 public class NotificationUtils {
     private static Image image = Toolkit.getDefaultToolkit().getImage(""); // TODO: Get this working
-    private static TrayIcon trayIcon = new TrayIcon(image, "Spigot Notifier");
+    private static TrayIcon trayIcon = new TrayIcon(image, "XenForo Notifier");
 
     public static void createNotification(String mainText, String message) { // TODO: Make sure this is thread-safe, use TrayNotification notifications for OS other than Windows
         if (SystemTray.isSupported()) {
-            try {
-                SystemTray.getSystemTray().add(trayIcon); // TODO: Prevent this from duplicating
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (!trayIconExists()) {
+                try {
+                    SystemTray.getSystemTray().add(trayIcon);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             trayIcon.displayMessage(mainText, message, MessageType.NONE);
         }
+    }
+
+    private static boolean trayIconExists() {
+        for (TrayIcon _trayIcon : SystemTray.getSystemTray().getTrayIcons()) {
+            if (trayIcon == _trayIcon) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
