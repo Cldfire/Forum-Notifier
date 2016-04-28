eaa3f2df4a02def40d1ee1067d1bfb4093d3642c
Scott <scott-scheiner@hotmail.com>
2016-04-28 21:28:11 +0930

    Make LangUtils 5.2 times better.

--- src/main/java/com/cldfire/xenforonotifier/util/LangUtils.java
+++ src/main/java/com/cldfire/xenforonotifier/util/LangUtils.java
@@ -6,35 +6,45 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class LangUtils { // TODO: Clean this up, rushed it to get repo online
 
     private static Properties properties;
 
-    public enum Locale {
-        EN_US("en_US", "English (US)"),
-        EN_TA("en_TA", "Taco"); // Add more
+    /**
+     * Cheers for Thinkofname for making this 5 times faster than my version
+     *
+     * @param property key per se
+     * @param replacey (Optional) if the provided {@code property} contains "{0}" this method will replace them with
+     *                 strings that are relative to the array {@code replacey}
+     * @return Translated String
+     */
+    public static String translate(String property, String... replacey) {
+        String translate = properties.getProperty(property);
+        if (translate.contains("{0}")) {
+            int cap = property.length() + Arrays.stream(replacey).mapToInt(String::length).sum();
+            StringBuilder builder = new StringBuilder(cap);
+            int index = 0;
+            int offset = 0;
+            int pos = 0;
+            while (index < replacey.length && (pos = translate.indexOf("{0}", offset)) != -1) {
+                builder.append(translate, offset, pos);
+                builder.append(replacey[index++]);
+                offset = pos + 3;
+            }
+            builder.append(translate, offset, translate.length());
+            return builder.toString();
 
-        private final String tag;
-        private final String name;
-        Locale(String tag, String name) {
-            this.tag = tag;
-            this.name = name;
         }
-        public String getTag() {
-            return this.tag;
-        }
-        public String getName() {
-            return this.name;
-        }
+        return translate;
     }
 
-    // Returns localised text
-    public static String translate(String property) {
-        return properties.getProperty(property, property);
-    }
-
+    /**
+     * Loads the locale
+     * @param locale {@link Locale}
+     */
     public static void loadLocale(Locale locale) {
         properties = new Properties();
 
@@ -48,6 +58,26 @@ public class LangUtils { // TODO: Clean this up, rushed it to get repo online
             System.out.println("Loaded language " + locale.getTag());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public enum Locale {
+        EN_US("English (US)"),
+        EN_TA("Taco"); // Add more
+
+        private final String name;
+
+        Locale(String name) {
+            this.name = name;
+        }
+
+        public String getTag() {
+            String[] nameSplit = this.name().split("_");
+            return nameSplit[0].toLowerCase() + "_" + nameSplit[1];
+        }
+
+        public String getName() {
+            return this.name;
         }
     }
 
