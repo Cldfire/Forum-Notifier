f391bfa2ed92915e5675fa4dcb87e698e7baef82
Cldfire <cldfire@3grid.net>
2016-05-06 13:13:49 -0400

    Infect the program with more blobs

--- src/main/java/com/cldfire/xenforonotifier/util/animations/ParticleAnimation.java
+++ src/main/java/com/cldfire/xenforonotifier/util/animations/ParticleAnimation.java
@@ -1,6 +1,10 @@
 package com.cldfire.xenforonotifier.util.animations;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
 import javafx.animation.PathTransition;
+import javafx.animation.ScaleTransition;
+import javafx.application.Platform;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -10,25 +14,62 @@ import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Cldfire on 5/2/2016.
  */
 public class ParticleAnimation {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public ParticleAnimation(Pane layer) {
-        Circle test = createParticle(20.0, Color.AQUAMARINE);
-        layer.getChildren().add(test);
-        animate(test, generateRandomPath(layer));
+        //executor.submit(() -> {
+            Random randGen = new Random();
+
+            for (int i = randGen.nextInt(30) * 10 + 20; i > 0; i--) {
+                Circle test = createParticle(1.0, Color.AQUAMARINE, layer);
+               // Platform.runLater(() -> {
+                    layer.getChildren().add(test);
+               // });
+                animate(test, generateRandomPath(layer));
+                try {
+                    Thread.sleep(randGen.nextInt(5));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+       // });
     }
 
-    private Circle createParticle(Double radius, Color color) {
-        return new Circle(radius, color);
+    private Circle createParticle(Double radius, Color color, Pane layer) {
+        Random randGen = new Random();
+
+        Circle returnCircle = new Circle(radius, color);
+        returnCircle.setOpacity(0.0);
+        returnCircle.setTranslateX(randGen.nextInt(800));
+        returnCircle.setTranslateY(randGen.nextInt(500));
+        return returnCircle;
     }
 
     private void animate(Circle particle, Path path) {
-        PathTransition pathTransition = new PathTransition(Duration.minutes(1.0), path, particle);
+        Random randGen = new Random();
+
+        PathTransition pathTransition = new PathTransition(Duration.seconds(path.getElements().size() * (randGen.nextInt(10) + 9)), path, particle);
+        pathTransition.setInterpolator(Interpolator.EASE_OUT);
+
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(3f), particle);
+        scaleTransition.setToX(10f);
+        scaleTransition.setToY(10f);
+        scaleTransition.setInterpolator(Interpolator.EASE_OUT);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(6f), particle);
+        fadeTransition.setToValue(0.7);
+        fadeTransition.setInterpolator(Interpolator.EASE_OUT);
+
         pathTransition.play();
+        scaleTransition.play();
+        fadeTransition.play();
     }
 
     private Path generateRandomPath(Pane layer) {
